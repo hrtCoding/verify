aa07d5591fc92af80c0f6f49d11af08f5cc14c81
hrtCoding <86431981+hrtCoding@users.noreply.github.com>
2022-08-15 16:44:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key <= 9) {
+      codes[idx].value = "";
+      setTimeout(() => {
+        codes[idx + 1].focus();
+      }, 10);
+    } else if (e.key === "backspace") {
+      setTimeout(() => {
+        codes[idx - 1].focus();
+      });
+    }
+  });
+});
